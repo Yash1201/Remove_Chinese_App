b38bab56b54fc82cab5543b4557b920bb3ff173e
Yash1201 <yashraj.50thecool@gmail.com>
2020-06-07 00:53:15 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:intent/intent.dart' as android_intent;
 import 'package:intent/action.dart' as android_action;
 
-void main() => runApp(MaterialApp(home: ListAppsPages()));
+void main() => runApp(MaterialApp(title: "Remove Chinese App",debugShowCheckedModeBanner: false,home: ListAppsPages()));
 
 class ListAppsPages extends StatefulWidget {
   @override
@@ -20,19 +20,27 @@ class _ListAppsPagesState extends State<ListAppsPages> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+//      backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: Text("Chinese Installed applications"),
+        title: Text(" Remove Chinese App"),
+
+        actions: <Widget>[
+
+          IconButton(icon: Icon(Icons.share), onPressed: () {}),
+          SizedBox(width: 15,),
+        ],
       ),
       body: _ListAppsPagesContent(
           includeSystemApps: _showSystemApps,
           onlyAppsWithLaunchIntent: _onlyLaunchableApps,
           key: GlobalKey(),
-
       ),
       bottomNavigationBar: BottomAppBar(
+//        color: Colors.grey,
         child: Container(
           height: 60,
           padding: EdgeInsets.only(left: 80,right: 80,top: 5,bottom: 5),
+
           child: FloatingActionButton.extended(
 
             onPressed: () {
@@ -99,11 +107,57 @@ class _ListAppsPagesContent extends StatelessWidget {
             List<Application> chineseapps = data.data;
             List<Application> apps = List<Application>();
             List<String> myList = List<String>();
-            myList.add("App Manager");
-            myList.add("Skype");
-            myList.add("Tik Tok");
-            myList.add("CamScanner");
-            myList.add("NIT Results");
+
+
+
+
+
+
+
+            myList.add('Helo');
+            myList.add('Likee');
+            myList.add('Helo Lite');
+            myList.add('UVideo');
+            myList.add('Likee Lite');
+            myList.add('Mi Remote');
+            myList.add('Mi Store');
+            myList.add('HAGO');
+            myList.add('SHAREit');
+            myList.add('TikTok');
+            myList.add('TikTok Lite');
+            myList.add('Kwai');
+            myList.add('UC Browser');
+            myList.add('UC Mini');
+            myList.add('Turbo VPN');
+            myList.add('LiveMe');
+            myList.add('Bigo Live');
+            myList.add('Vigo Video');
+            myList.add('Vigo Lite');
+            myList.add('UDictionary');
+            myList.add('Flash Keyboard');
+            myList.add('BeautyPlus');
+            myList.add('Xender');
+            myList.add('CamScanner');
+            myList.add('PUBG MOBILE');
+            myList.add('PUBG MOBILE LITE');
+            myList.add('Clash of Kings');
+            myList.add('Mobile Legends');
+            myList.add('Club Factory');
+            myList.add('SHEIN');
+            myList.add('ROMWE');
+            myList.add('Xiaomi Home');
+            myList.add('realme store');
+            myList.add('AppLock');
+            myList.add('VMate');
+            myList.add('Game of Sultans');
+            myList.add('Mafia City');
+            myList.add('Battle of Empires');
+            myList.add('WPS Office');
+
+
+
+
+
 
 
 
@@ -117,22 +171,36 @@ class _ListAppsPagesContent extends StatelessWidget {
                 msg: '${apps.length} App Found.',
                 toastLength: Toast.LENGTH_SHORT,
                 gravity: ToastGravity.BOTTOM,
-                backgroundColor: Colors.black,
-                textColor: Colors.red,
+                backgroundColor: Colors.red,
+                textColor: Colors.white,
 
             );
 
 
 
+            return apps.isEmpty ?
+            Center(
 
-            return ListView.builder(
-
+                child: Text(
+                  'WOW ! No Chinese App Found \n'
+                      '\n'
+                      '#Boycott China\n'
+                      '\n'
+                      'Proud to be Indian',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+            ) :
+            ListView.builder(
                 itemBuilder: (context, position) {
                   Application app = apps[position];
 
                   return Column(
                     children: <Widget>[
                       ListTile(
+
                         contentPadding: EdgeInsets.all(10),
                         leading: app is ApplicationWithIcon
                             ? CircleAvatar(
@@ -167,6 +235,7 @@ class _ListAppsPagesContent extends StatelessWidget {
                           onPressed: () async {
 
                             String ck = app.packageName;
+                            String an = app.appName;
                             android_intent.Intent()
                               ..setAction(android_action.Action.ACTION_DELETE)
                               ..setData(Uri.parse("package:${app.packageName}"))
@@ -179,16 +248,18 @@ class _ListAppsPagesContent extends StatelessWidget {
 //                                print(ck);
 //                                print("Uninstalled Succesfull");
 //                                print(isInstalled);
-                                if(isInstalled == true) {
+                                if(isInstalled == false) {
 
 //                                  print('yash');
 //                                  print(ck);
                                   Fluttertoast.showToast(
-                                    msg: 'Congrats ! App Deleted , Rescan Again',
+                                    msg: '${an} App Deleted ,'
+                                        ' Rescan Again',
                                     toastLength: Toast.LENGTH_LONG,
                                     gravity: ToastGravity.BOTTOM,
-                                    backgroundColor: Colors.black,
-                                    textColor: Colors.green,
+                                    backgroundColor: Colors.green,
+                                    textColor: Colors.white
+
                                   );
                                   apps.removeAt(position);
 
